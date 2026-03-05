00c7658d32f2eab34fd3dd7795f70e57dcde73e9
Lonnie Huggins <zavarushka7@gmail.com>
2026-03-05 16:15:57 +0500

    сделала задание 2: неявный intent

---
+++ app/src/main/java/com/example/vkeducation/FirstActivity.kt
@@ -0,0 +1,46 @@
+package com.example.vkeducation
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.vkeducation.databinding.ActivityFirstBinding
+
+
+
+class FirstActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.openSecondActivityBtn.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("TEXT_FROM_FIRST_ACTIVITY", binding.plainTextInput.text.toString())
+            startActivity(intent)
+        }
+        binding.callFriendBtn.setOnClickListener {
+            val phoneNumber = binding.plainTextInput.text.toString().trim()
+            if (!isValidPhone(phoneNumber)){
+                Toast.makeText(this, "Введен неправильный номер телефона", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${phoneNumber}")
+            }
+
+            startActivity(intent)
+
+        }
+    }
+
+
+}
+private fun isValidPhone(phone: String): Boolean {
+    val regex = Regex("^8\\d{10}$|^[+]?7\\d{10}$")  // Только 8xxx и +7xxx
+    return regex.matches(phone)
+}
