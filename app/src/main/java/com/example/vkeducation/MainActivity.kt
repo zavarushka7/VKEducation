c50277e2b8b3670ef421e0d5b0dc93e6e36fdaf4
zavarushka7 <zavarushka7@gmail.com>
2026-03-08 11:54:59 +0500

    сделала основу для Compose

--- app/src/main/java/com/example/vkeducation/MainActivity.kt
+++ app/src/main/java/com/example/vkeducation/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.vkeducation.ui.theme.VKEducationTheme
 
@@ -19,29 +21,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VKEducationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                MainActivityScreen()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainActivityScreen() {
+    Column{
+        Row(){
+            Image(
+                painter = painterResource( R.drawable.img),
+                contentDescription = null,
+            )
+            Text(
+                ""
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     VKEducationTheme {
-        Greeting("Android")
+        MainActivityScreen()
     }
 }
