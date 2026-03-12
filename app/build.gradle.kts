c65d4ed79b382f336a00e915b2fecbb2afd5bdd4
Lonnie Huggins <zavarushka7@gmail.com>
2026-03-12 16:16:18 +0500

    сделала основной макет экрана

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+
+
+
+
 }
